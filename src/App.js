f12408439b765521194f7adbdb3419e851c8f7b8
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-19 02:30:36 -0800

    Using object

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import Content from './Content';
 import Total from './Total';
 
 const App = () => {
-  const course = 'Half Stack application development';
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
-
+  const { name, parts } = course;
   return (
     <>
-      <Header course={course} />
+      <Header course={name} />
       <Content parts={parts} />
       <Total parts={parts} />
     </>
