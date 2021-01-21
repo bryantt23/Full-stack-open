688cc78872e6eff88f3025449047e49761c08db9
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-20 22:02:11 -0800

    bold total number of courses

--- src/Total.js
+++ src/Total.js
@@ -4,5 +4,9 @@ export default function Total(parts) {
   console.log(parts);
   const counts = parts.parts.map(elem => elem.exercises);
   const total = counts.reduce((acc, cur) => acc + cur, 0);
-  return <p>Number of exercises {total}</p>;
+  return (
+    <p>
+      <b>total of {total} exercses</b>
+    </p>
+  );
 }
